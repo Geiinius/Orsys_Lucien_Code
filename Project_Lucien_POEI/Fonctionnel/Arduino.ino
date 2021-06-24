4990b932abc980d3d4c6fcf49df205f4419e54e3
MANZA CAPITAO Lucien <lucien.manza@gmail.com>
2021-06-24 03:19:10 -0700

    MaJ des programmes fonctionnel POEI

---
+++ Project_Lucien_POEI/Fonctionnel/Arduino.ino
@@ -0,0 +1,108 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+
+volatile int rSize = 0;
+volatile bool finish = false;
+int nb_people = 0;
+char myArray[100];
+volatile uint8_t lum =0;
+uint8_t addr_slave  = 0x18 ;
+
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+
+void setup()
+{
+Serial.begin(9600);
+Wire.begin();
+Wire.beginTransmission(addr_slave);
+  lcd.init();                      // initialize the lcd 
+
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(1,0);
+  
+  //SPI
+  pinMode(MISO, OUTPUT);
+  pinMode(SCK, INPUT);
+  SPCR |= bit(SPE); // turn on SPI in slave mode
+  SPI.attachInterrupt(); // turn on interrupts
+  Serial.println("Initialization done...");
+
+  
+}
+
+ISR (SPI_STC_vect) // SPI interrupt routine
+{
+  myArray[rSize] = SPDR;
+  if (myArray[rSize] == '!') {
+    lum =map(analogRead(A0), 0, 1023, 0, 254);
+    SPDR = lum;
+    finish = true;}
+     /*if (myArray[rSize] == '?') {
+    SPDR = 0xFF;
+    finish = true;}*/
+   // myArray[rSize] = SPDR;
+  
+  rSize++;
+} // end of interrupt service routine (ISR) for SPI
+
+
+
+void loop()
+{
+  
+  /************************************************
+   *               I2C Temperature                *
+   ***********************************************/
+  
+  byte temperatureMSB = 0;
+  byte temperatureLSB = 0;
+  float temperature=0.0;
+  Wire.beginTransmission(addr_slave);
+  Wire.write(0x05);
+  Wire.endTransmission(false);  // Condition RESTART
+  Wire.requestFrom(0x18, 2); // Deux octets sont requis
+  Wire.endTransmission();
+  if (2 <= Wire.available()) {
+    temperatureMSB = Wire.read();  // Octet de poids fort
+    temperatureLSB = Wire.read();  // Octet de poids faible
+    temperature =(16*(temperatureMSB&0xF)+0.0625*(temperatureLSB&0xFF));
+  }
+
+
+  /************************************************
+   *                      SPI                     *
+   ***********************************************/
+
+
+  /************************************************
+   *                      LCD                     *
+   ***********************************************/
+  if(finish){
+    if((char)myArray[0]=='0'){
+      lcd.noBacklight(); //eteindre le retro-eclairage
+    }
+    else{
+    Serial.print("Temperature = ");
+    Serial.println(temperature);
+    lcd.backlight();
+    lcd.setCursor(0,0);
+    lcd.print("T : ");
+    lcd.print(temperature);
+    lcd.print(" C");
+    lcd.setCursor(0,1);
+    lcd.print("How many ppl? :");
+    lcd.print((char)myArray[0]);
+    Serial.println(temperatureLSB);
+    Serial.println(temperatureMSB);
+    Serial.println(map(analogRead(A0), 0, 1024, 0, 255));
+    rSize = 0;
+    finish = false;
+    }
+  }
+ 
+ }
---
+++ Project_Lucien_POEI/RaspberryGood_2.cpp
@@ -0,0 +1,151 @@
+//============================================================================
+// Name        : Project_POEI.cpp
+// Author      : Lucien
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <cctype>
+#include <thread>
+#include <chrono>
+#include <pigpio.h>
+#include <unistd.h>
+#include "mqtt/async_client.h"
+
+#define O_LED 17
+#define O_LED2 23
+#define BUZZER_PWM 18
+#define LED_PWM 12
+using namespace std;
+uint16_t valeur;
+uint nb=0;
+const string SERVER_ADDRESS { "tcp://192.168.0.25:1883" };
+const string CLIENT_ID { "pi" };
+int pwmValue_init =0;
+int pwmValue_alert =20;
+/////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char *argv[]) {
+
+	if (gpioInitialise() < 0) {
+		cout << "Error initializing pigpio library..." << endl;
+		exit(-1);
+	}
+
+	//MQTT
+	mqtt::async_client cli(SERVER_ADDRESS, CLIENT_ID);
+
+	//SPI
+	uint32_t flags = 0x00;
+	int hSpi = spiOpen(0, 100000, flags);
+	if (hSpi > 0) {
+		cout << "Error SPI initialization..." << endl;
+		gpioTerminate();
+		exit(-1);
+	}
+	//char sBuffer[3];
+	//sBuffer[0]=nb;sBuffer[1]='!';sBuffer[2]=' ';
+	const char *sBuffer = "! ";
+	char rBuffer[100];
+
+	//Buzzer
+	gpioSetMode(BUZZER_PWM, PI_OUTPUT);
+	gpioPWM(BUZZER_PWM, pwmValue_init);
+	gpioSetMode(LED_PWM,PI_OUTPUT);
+	gpioPWM(LED_PWM,pwmValue_init);
+	try {
+// Connect to the server
+		cout << "Connecting to the MQTT server..." << flush;
+		cli.start_consuming();
+		//cli.connect()->wait();
+		//cli.subscribe("Test", 1)->wait();
+		auto rsp = cli.connect()->get_connect_response();
+		// If there is no session present, then we need to subscribe, but if
+		// there is a session, then the server remembers us and our subscriptions.
+		if (!rsp.is_session_present())
+			cli.subscribe("Test", 1)->wait();
+		if (!rsp.is_session_present())
+			cli.subscribe("Nombre", 1)->wait();
+		if (!rsp.is_session_present())
+			cli.subscribe("Alerte", 1)->wait();
+
+		while (1) {
+
+			/* Methode bloquante */
+
+			//auto msg = cli.consume_message();
+			/*Methode non bloquante*/
+
+			mqtt::const_message_ptr msg;
+
+
+			bool verif = cli.try_consume_message(&msg);
+			if (verif) {
+				if(msg->get_topic()=="Nombre/add"){
+						cout << "Nombre: " << msg->to_string() << endl;
+						if(nb<6)
+							nb=nb+1;
+				}
+				if(msg->get_topic()=="Nombre/sub"){
+						cout << "Nombre: " << msg->to_string() << endl;
+						if(nb<6)
+							nb=nb-1;
+				}
+				if(msg->get_topic()=="Alerte"){
+					if(msg->to_string()=="ALERTE! INTRUS!")
+						gpioPWM(BUZZER_PWM, pwmValue_alert);
+					if(msg->to_string()=="STOP")
+						gpioPWM(BUZZER_PWM, pwmValue_init);
+					cout << msg->to_string() << endl;
+				}
+
+			}
+			sleep(1);
+			char rBuffer[100];
+			char sBuffer[2];
+			sBuffer[0] = (char)('0'+nb);
+			sBuffer[1]='!';
+			cout << "Nombre :" << nb << endl;
+			spiXfer(hSpi, (char*) sBuffer, (char*) rBuffer, strlen(sBuffer));
+			//int value = (int) rBuffer[0];
+			cout << "Received : "<< (int)rBuffer[0]<< endl;
+			sleep(1);
+
+			if(rBuffer[0] <= 60)
+				gpioPWM(LED_PWM,245);
+			if(rBuffer[0]>= 140)
+				gpioPWM(LED_PWM, 0);
+			if(rBuffer[0]<140 && rBuffer[0]>60)
+				gpioPWM(LED_PWM, 20);
+
+		}
+	} catch (const mqtt::exception &exc) {
+		cerr << "\n " << exc << endl;
+		return 1;
+	}
+
+	cout << "\nNow, the program is connected to the broker" << flush;
+
+	try {
+		if (cli.is_connected()) {
+			cout << "\nShutting down and disconnecting from the MQTT server..."
+					<< flush;
+			cli.disconnect()->wait();
+			cout << "\nNow it is disconnected..." << flush;
+		} else {
+			cout << "\nClient was already disconnected" << flush;
+		}
+		spiClose(hSpi);
+		gpioTerminate();
+	} catch (const mqtt::exception &exc) {
+		cerr << "\n " << exc << endl;
+		return 1;
+	}
+
+	return 0;
+}
